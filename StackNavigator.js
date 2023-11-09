ba188e839feb679a0979b04de34969b0e3b87727
chengivan2 <chengoleivan@gmail.com>
2023-11-09 20:03:49 +0300

    Initial commit

---
+++ StackNavigator.js
@@ -0,0 +1,26 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+
+
+
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {    
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+                <Stack.Screen name="Productscreen" component={ProductScreen} options={{headerShown: false}} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default StackNavigator;
